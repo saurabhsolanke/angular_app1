8d07c789600d444f4d0d836b54747a58bd0a0a66
Saurabh Solanke <35625425+saurabhsolanke@users.noreply.github.com>
2022-03-20 13:18:53 +0530

    first commit

---
+++ src/app/warning_alert/warning_alert.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warning-alert';
+    template: `
+    <p>helo warning alert</p>
+    `
+    
+})
+
+export class WarningAlertComponent {
+
+}
